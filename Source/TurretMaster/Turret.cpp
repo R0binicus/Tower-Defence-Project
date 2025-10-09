7c85f73ee81915f3030ef59e535c9863824ada5c
Robin <robinandloki@gmail.com>
2025-10-09 13:24:04 +1100

    Basic Turret Tracking: Turret C++ GetClosestEnemy
    - Setup working C++ GetClosestEnemy
    - Uses sphere trigger collider to find enemies to check distance of

--- Source/TurretMaster/Turret.cpp
+++ Source/TurretMaster/Turret.cpp
@@ -6,22 +6,82 @@
 // Sets default values
 ATurret::ATurret()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    RangeSphere = CreateDefaultSubobject<USphereComponent>(TEXT("RangeSphere"));
+
+    RangeSphere->SetCollisionProfileName(TEXT("EnemyDetection"));
+    RangeSphere->SetGenerateOverlapEvents(true);
+    RangeSphere->SetSphereRadius(TurretRange);
+
+
+    RangeSphere->OnComponentBeginOverlap.AddDynamic(this, &ATurret::OnOverlapBegin);
+    RangeSphere->OnComponentEndOverlap.AddDynamic(this, &ATurret::OnOverlapEnd);
 }
 
 // Called when the game starts or when spawned
 void ATurret::BeginPlay()
 {
 	Super::BeginPlay();
-	
+    TurretLocation = GetActorLocation();
 }
 
 // Called every frame
 void ATurret::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+    CurrentClosestEnemy = GetClosestEnemy();
 
+    if (CurrentClosestEnemy)
+    {
+
+    }
+    
+}
+
+void ATurret::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    
+    EnemyRefArray.Add(OtherActor);
+}
+
+void ATurret::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    EnemyRefArray.Remove(OtherActor);
+
+}
+
+AActor* ATurret::GetClosestEnemy()
+{
+    TObjectPtr<AActor> PotentialClosestEnemy = nullptr;
+    float CurrentClosestDistance;
+
+    for (size_t i = 0; i < EnemyRefArray.Num(); i++)
+    {
+        if (!EnemyRefArray[i])
+        {
+            continue;
+        }
+
+        if (!PotentialClosestEnemy)
+        {
+            CurrentClosestDistance = FVector::DistSquared(EnemyRefArray[i]->GetActorLocation(), TurretLocation);
+            PotentialClosestEnemy = EnemyRefArray[i];
+        }
+        else
+        {
+            float EnemyDistance = FVector::DistSquared(EnemyRefArray[i]->GetActorLocation(), TurretLocation);
+
+            if (EnemyDistance <= CurrentClosestDistance)
+            {
+                continue;
+            }
+
+            CurrentClosestDistance = EnemyDistance;
+            PotentialClosestEnemy = EnemyRefArray[i];
+        }
+    }
+
+    return PotentialClosestEnemy;
 }
 
--- Source/TurretMaster/Turret.h
+++ Source/TurretMaster/Turret.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
 #include "Turret.generated.h"
 
 UCLASS()
@@ -12,15 +13,37 @@ class TURRETMASTER_API ATurret : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	ATurret();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turret")
+	USphereComponent* RangeSphere;
+
+	
+
 protected:
-	// Called when the game starts or when spawned
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turret")
+	float TurretRange = 3000.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Turret")
+	FVector TurretLocation;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> EnemyRefArray;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Turret")
+	TObjectPtr<AActor> CurrentClosestEnemy;
+
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintCallable, Category = "Turret",
+		meta = (ToolTip = "Returns the closest enemy in the RangeSphere"))
+	AActor* GetClosestEnemy();
 };
