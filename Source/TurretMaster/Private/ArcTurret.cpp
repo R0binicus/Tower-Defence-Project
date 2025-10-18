7d474b5eae326b5e5784c05b914aad2f6326bd4a
Robin <robinandloki@gmail.com>
2025-10-18 22:32:56 +1100

    Arc Turret: Basic turret arc shooting
    - Implemented very basic arc shooting
    - Doesn't have 100% accuracy
    - Shoots EXTREMELY high to hit the enemy, and takes way too long
    - Low angle doesn't work, only high angle

--- Source/TurretMaster/Private/ArcTurret.cpp
+++ Source/TurretMaster/Private/ArcTurret.cpp
@@ -10,18 +10,41 @@ float AArcTurret::FindDesiredPitch()
 
     // Prevent turret from aiming vertically  
     // if the vertical distance is too great
-    if (TargetDotProduct < GiveUpVerticalAimThreshold)
+    /*if (TargetDotProduct < GiveUpVerticalAimThreshold)
     {
         return InitialRotation.Pitch;
-    }
+    }*/
 
     const float TurretCurrentPitch = CurrentTurretRotation.Pitch;
     const float TargetPitchDifference = FMath::RadiansToDegrees(TargetDirection.Z - MuzzleForward.Z);
     const float TurretDesiredPitch = TurretCurrentPitch + TargetPitchDifference;
 
-    // Used to make the turret continue partially faceing the enemy, 
-    // when the enmy is only just outside the turret's aiming bounds
-    return FMath::Clamp(TurretDesiredPitch, AimVerticalLowerBound, AimVerticalUpperBound);
+
+
+
+
+    const float V = ProjectileSpeed;
+    const float G = 980.f;
+    const FVector MuzzleLocation = MuzzleDirectionSocket->GetComponentLocation();
+    FVector PlaneTarget = TargetLocation;
+    PlaneTarget.Z = MuzzleLocation.Z;
+
+    const float X = FVector::Distance(MuzzleLocation, PlaneTarget);
+    const float Y = MuzzleLocation.Z - TargetLocation.Z;
+
+
+    const float V2 = V * V;
+    const float V4 = V2 * V2;
+    const float X2 = X * X;
+    float SQ = (V4 - G * ((G * X2) + (2.0 * Y * V2)));
+
+    SQ = sqrt(SQ);
+
+    const float Angle = atan2((V*V + SQ), (G * X));
+
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Angle: %f"), FMath::RadiansToDegrees(Angle)));
+
+    return FMath::RadiansToDegrees(Angle);
 }
 
 bool AArcTurret::CanShoot()
