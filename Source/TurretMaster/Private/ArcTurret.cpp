07ef3bc55b2b1b4c3319211130e3287c345741d2
Robin <robinandloki@gmail.com>
2025-10-18 20:54:37 +1100

    Arc Turret: Created ArcTurret scripts

---
+++ Source/TurretMaster/Private/ArcTurret.cpp
@@ -0,0 +1,2 @@
+#include "ArcTurret.h"
+
---
+++ Source/TurretMaster/Public/ArcTurret.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Turret.h"
+#include "ArcTurret.generated.h"
+
+/**
+ * Variant of the turret, which shoots it's projectiles in an arc towards 
+ * it's intended target, rather than a straight line towards it
+ */
+UCLASS()
+class TURRETMASTER_API AArcTurret : public ATurret
+{
+	GENERATED_BODY()
+	
+};
