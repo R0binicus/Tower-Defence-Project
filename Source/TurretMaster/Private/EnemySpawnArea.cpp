977f10fe5203b0bc2721250f45875eb3d592e5ac
Robin <robinandloki@gmail.com>
2025-10-30 16:06:20 +1100

    Basic Enemy Spawning: Simple BeginPlay enemy spawner
    - Simple spawner, which creates enemies at BeginPlay
    - The spawner creates the enemies inside a spawn volume, which is based on a box component
    - Spawner creates the enemies at random point in volume, with random rotation

--- Source/TurretMaster/Private/EnemySpawnArea.cpp
+++ Source/TurretMaster/Private/EnemySpawnArea.cpp
@@ -3,15 +3,50 @@
 AEnemySpawnArea::AEnemySpawnArea()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	SpawnVolumeBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Spawn Volume Box"));
+	SpawnVolumeBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SpawnVolumeBox->SetBoxExtent(FVector(1000, 1000, 0), false);
 }
 
 void AEnemySpawnArea::BeginPlay()
 {
 	Super::BeginPlay();
+
+    World = GetWorld();
+	SpawnVolume = SpawnVolumeBox->GetUnscaledBoxExtent();
+
+    SpawnEnemy(EnemyToSpawn);
+    SpawnEnemy(EnemyToSpawn);
+    SpawnEnemy(EnemyToSpawn);
+    SpawnEnemy(EnemyToSpawn);
+    SpawnEnemy(EnemyToSpawn);
 }
 
-void AEnemySpawnArea::Tick(float DeltaTime)
+void AEnemySpawnArea::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AEnemySpawnArea::SpawnEnemy(const TSubclassOf<AActor> EnemyClass) const
+{
+    if (!EnemyClass)
+    {
+        return;
+    }
+
+    if (!World)
+    {
+        return;
+    }
+
+    const FRotator CollectibleRotation = FRotator(0.0f, FMath::RandRange(0.0f, 360.0f), 0.0f);
+
+    FVector SpawnLocation = GetActorLocation();
+    SpawnLocation.X += FMath::RandRange(-SpawnVolume.X, SpawnVolume.X);
+    SpawnLocation.Y += FMath::RandRange(-SpawnVolume.Y, SpawnVolume.Y);
+    SpawnLocation.Z += FMath::RandRange(-SpawnVolume.Z, SpawnVolume.Z);
+
+    const TObjectPtr<AActor> NewEnemy = World->SpawnActor<AActor>(EnemyClass, SpawnLocation, CollectibleRotation);
 }
 
--- Source/TurretMaster/Public/EnemySpawnArea.h
+++ Source/TurretMaster/Public/EnemySpawnArea.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
 #include "EnemySpawnArea.generated.h"
 
 UCLASS()
@@ -13,7 +14,23 @@ public:
 	AEnemySpawnArea();
 
 protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Enemy Spawn Area")
+	TObjectPtr<UBoxComponent> SpawnVolumeBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Spawn Area")
+	TSubclassOf<AActor> EnemyToSpawn;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Enemy Spawn Area")
+	FVector SpawnVolume;
+
+	UPROPERTY()
+	TObjectPtr<UWorld> World;
+
 	virtual void BeginPlay() override;
 
-	virtual void Tick(float DeltaTime) override;
+	virtual void Tick(const float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Enemy Spawn Area",
+		meta = (ToolTip = "Spawns an enemy class at a random point in the SpawnVolumeBox"))
+	void SpawnEnemy(const TSubclassOf<AActor> EnemyClass) const;
 };
