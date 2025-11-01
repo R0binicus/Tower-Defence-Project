ed9a61f2c72c29151e582c0fb9e321ccc6905fa0
Robin <robinandloki@gmail.com>
2025-11-01 13:17:10 +1100

    Basic Enemies: Updated dependencies
    - Updated build dependencies to include UMG, slate and the AI Module

--- Source/TurretMaster/TurretMaster.Build.cs
+++ Source/TurretMaster/TurretMaster.Build.cs
@@ -8,13 +8,10 @@ public class TurretMaster : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "AIModule" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
