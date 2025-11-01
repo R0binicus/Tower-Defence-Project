d92dfdd51ce6dd4592de1b69d0acb5b79cc60f38
Robin <robinandloki@gmail.com>
2025-11-01 12:53:24 +1100

    Basic Enemies: Parented bots to Enemy class
    - Bot blueprints are now parented to the AEnemy class
    - Changed EnemySpawnArea EnemyToSpawn to be AEnemy

--- Source/TurretMaster/Private/EnemySpawnArea.cpp
+++ Source/TurretMaster/Private/EnemySpawnArea.cpp
@@ -43,6 +43,6 @@ void AEnemySpawnArea::SpawnEnemy() const
     SpawnLocation.Y += FMath::RandRange(-SpawnVolume.Y, SpawnVolume.Y);
     SpawnLocation.Z += FMath::RandRange(-SpawnVolume.Z, SpawnVolume.Z);
 
-    const TObjectPtr<AActor> NewEnemy = World->SpawnActor<AActor>(EnemyToSpawn, SpawnLocation, CollectibleRotation);
+    const TObjectPtr<AEnemy> NewEnemy = World->SpawnActor<AEnemy>(EnemyToSpawn, SpawnLocation, CollectibleRotation);
 }
 
