fa1fb98117ff4d1ef56d56450c9deac433899eb2
Robin <robinandloki@gmail.com>
2025-11-05 09:58:36 +1100

    Advanced Wave System: Created LimitedRepeatTimer class

---
+++ Source/TurretMaster/Private/LimitedRepeatTimer.cpp
@@ -0,0 +1,2 @@
+#include "LimitedRepeatTimer.h"
+
---
+++ Source/TurretMaster/Public/LimitedRepeatTimer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "LimitedRepeatTimer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETMASTER_API ULimitedRepeatTimer : public UObject
+{
+	GENERATED_BODY()
+	
+};
