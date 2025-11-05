c0f1b96e3c18901157c4c1ecb7d1f3f1bfd63282
Robin <robinandloki@gmail.com>
2025-11-06 10:13:45 +1100

    Building Data Asset: Setup BuildingDataAsset variables

--- Source/TurretMaster/Private/DataAssets/BuildingDataAsset.cpp
+++
@@ -1,2 +0,0 @@
-#include "DataAssets/BuildingDataAsset.h"
-
--- Source/TurretMaster/Public/DataAssets/BuildingDataAsset.h
+++ Source/TurretMaster/Public/DataAssets/BuildingDataAsset.h
@@ -5,11 +5,29 @@
 #include "BuildingDataAsset.generated.h"
 
 /**
- * 
+ * Data asset for storing accessible information about each building type
  */
 UCLASS()
 class TURRETMASTER_API UBuildingDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BuildingDataAsset")
+	FText Name;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BuildingDataAsset")
+	FText Description;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BuildingDataAsset")
+	TObjectPtr<UTexture2D> Icon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BuildingDataAsset")
+	FColor Colour;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BuildingDataAsset")
+	TObjectPtr<USkeletalMesh> SkeletalMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BuildingDataAsset", meta = (MustImplement = "Buildable"))
+	TSubclassOf<AActor> Type;
 };
