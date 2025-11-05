afcd5a18cb83d1a40605b172d68fd1a3b09cdd1c
Robin <robinandloki@gmail.com>
2025-11-06 09:59:14 +1100

    Building Data Asset: Created BuildingDataAsset class

---
+++ Source/TurretMaster/Public/DataAssets/BuildingDataAsset.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "BuildingDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETMASTER_API UBuildingDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
