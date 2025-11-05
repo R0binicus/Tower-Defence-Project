9e7dda826474c0189ab07077bddc489b3d565550
Robin <robinandloki@gmail.com>
2025-11-05 11:49:27 +1100

    Advanced Wave System: LimitedRepeatTimer functionality
    - Setup working LimitedRepeatTimer

--- Source/TurretMaster/Private/LimitedRepeatTimer.cpp
+++ Source/TurretMaster/Private/LimitedRepeatTimer.cpp
@@ -1,2 +1,37 @@
 #include "LimitedRepeatTimer.h"
 
+//ULimitedRepeatTimer::ULimitedRepeatTimer()
+//{
+//}
+
+void ULimitedRepeatTimer::SetupTimer(TObjectPtr<UWorld> InWorld, FTimerDelegate& InCallerDelagate, float InRate, int32 InRepeats)
+{
+	if (InRepeats == 0)
+	{
+		return;
+	}
+
+	if (!InWorld)
+	{
+		return;
+	}
+
+	World = InWorld;
+	RemainingRepeats = InRepeats;
+
+	TimerDelagate.BindUObject(this, &ULimitedRepeatTimer::RepeatFunction, InCallerDelagate, InRate);
+
+	InWorld->GetTimerManager().SetTimer(TimerHandle, TimerDelagate, InRate, false);
+}
+
+void ULimitedRepeatTimer::RepeatFunction(FTimerDelegate CallerDelagate, float RepeatRate)
+{
+	CallerDelagate.Execute();
+
+	if (!World)
+	{
+		return;
+	}
+
+	World->GetTimerManager().SetTimer(TimerHandle, TimerDelagate, RepeatRate, false);
+}
--- Source/TurretMaster/Public/LimitedRepeatTimer.h
+++ Source/TurretMaster/Public/LimitedRepeatTimer.h
@@ -1,15 +1,31 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
 #include "UObject/NoExportTypes.h"
 #include "LimitedRepeatTimer.generated.h"
 
 /**
- * 
+ * A wrapper around a timer, which is set to repeat a timer a set number of times
  */
 UCLASS()
 class TURRETMASTER_API ULimitedRepeatTimer : public UObject
 {
 	GENERATED_BODY()
-	
+
+public:
+	const FTimerHandle* GetTimerHandle() const { return &TimerHandle; };
+
+	void SetupTimer(TObjectPtr<UWorld> InWorld, FTimerDelegate& InCallerDelagate, float InRate, int32 InRepeats);
+
+protected:
+	int32 RemainingRepeats;
+
+	FTimerHandle TimerHandle;
+
+	FTimerDelegate TimerDelagate;
+
+	TObjectPtr<UWorld> World;
+
+	void RepeatFunction(FTimerDelegate CallerDelagate, float RepeatRate);
 };
