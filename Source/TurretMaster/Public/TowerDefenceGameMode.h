cee692bd1acb521da333d1b48c2d37c586f502a9
Robin <robinandloki@gmail.com>
2025-11-06 11:49:12 +1100

    Basic Building UI: Created TowerDefenceGameMode class

---
+++ Source/TurretMaster/Public/TowerDefenceGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerDefenceGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETMASTER_API ATowerDefenceGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
