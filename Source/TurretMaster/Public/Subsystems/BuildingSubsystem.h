9dc6e4a40cb796e547c76518299ce48b3e2e9799
Robin <robinandloki@gmail.com>
2025-11-06 11:01:15 +1100

    Basic Building UI: Created BuildingSubsystem class

---
+++ Source/TurretMaster/Public/Subsystems/BuildingSubsystem.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "BuildingSubsystem.generated.h"
+
+/**
+ * Subsystem for managing resources building structures
+ */
+UCLASS()
+class TURRETMASTER_API UBuildingSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
