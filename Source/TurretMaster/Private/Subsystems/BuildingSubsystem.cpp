3b125532ae6dcbf7ab0d18d4c4da8aaaa0466004
Robin <robinandloki@gmail.com>
2025-11-07 13:55:39 +1100

    Basic Building UI: Setup BuildingSubsystem declarations
    - Setup BuildingSubsystem function declarations
    - No implementation

--- Source/TurretMaster/Private/Subsystems/BuildingSubsystem.cpp
+++ Source/TurretMaster/Private/Subsystems/BuildingSubsystem.cpp
@@ -1,2 +1,16 @@
 #include "Subsystems/BuildingSubsystem.h"
 
+void UBuildingSubsystem::SelectedPlaceBuilding(UBuildingDataAsset* BuildingData)
+{
+
+}
+
+void UBuildingSubsystem::CancelPlaceBuilding()
+{
+
+}
+
+void UBuildingSubsystem::BuildingPlaced()
+{
+	CancelPlaceBuilding();
+}
--- Source/TurretMaster/Public/Subsystems/BuildingSubsystem.h
+++ Source/TurretMaster/Public/Subsystems/BuildingSubsystem.h
@@ -2,14 +2,33 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/WorldSubsystem.h"
+#include "DataAssets/BuildingDataAsset.h"
 #include "BuildingSubsystem.generated.h"
 
 /**
- * Subsystem for managing resources building structures
+ * Subsystem for managing resources and building structures
  */
 UCLASS()
 class TURRETMASTER_API UBuildingSubsystem : public UWorldSubsystem
 {
 	GENERATED_BODY()
+
 	
+public:
+	UFUNCTION(BlueprintCallable, Category = "BuildingSubsystem",
+		meta = (ToolTip = "Sets the building subsystem's CurrentPlaceBuildingSelected variable. Used when a player selects the place building button"))
+	void SelectedPlaceBuilding(UBuildingDataAsset* BuildingData);
+
+	UFUNCTION(BlueprintCallable, Category = "BuildingSubsystem",
+		meta = (ToolTip = "Clears the CurrentPlaceBuildingSelected"))
+	void CancelPlaceBuilding();
+
+	UFUNCTION(BlueprintCallable, Category = "BuildingSubsystem",
+		meta = (ToolTip = "Tells the building subsystem that a building is placed, then removes appropriate resources"))
+	void BuildingPlaced();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "BuildingSubsystem")
+	TObjectPtr<UBuildingDataAsset> CurrentPlaceBuildingSelected = nullptr;
+
 };
