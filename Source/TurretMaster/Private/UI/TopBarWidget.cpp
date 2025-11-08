f1c6d2f29470abc3851cc279c92cc174e386f331
Robin <robinandloki@gmail.com>
2025-11-08 18:10:04 +1100

    Basic Player HUD: Setup TopBarWidget declarations
    - Setup TopBarWidget's variable and function declarations

--- Source/TurretMaster/Private/UI/TopBarWidget.cpp
+++ Source/TurretMaster/Private/UI/TopBarWidget.cpp
@@ -1,5 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UI/TopBarWidget.h"
 
+void UTopBarWidget::NativeConstruct()
+{
+
+}
+
+void UTopBarWidget::NewWaveStarted(const int32 NewWavesRemaining, const int32 NewWaveEnemies)
+{
+
+}
+
+void UTopBarWidget::UpdateLivesText(const int32 NewLives)
+{
+
+}
+
+void UTopBarWidget::UpdateMoneyText(const int32 NewMoney)
+{
+
+}
+
+void UTopBarWidget::UpdateEnemiesRemainingText(const int32 NewEnemiesRemaining)
+{
+
+}
--- Source/TurretMaster/Public/UI/TopBarWidget.h
+++ Source/TurretMaster/Public/UI/TopBarWidget.h
@@ -1,17 +1,61 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+//#include "EnemyWaveData.h"
 #include "TopBarWidget.generated.h"
 
 /**
- * 
+ * Widget which is at the top of the screen. Displays information such as money and player lives
  */
 UCLASS()
 class TURRETMASTER_API UTopBarWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TopBarWidget")
+	int32 TotalWaveNum;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TopBarWidget")
+	int32 TotalWaveEnemies;
+
+	//TODO: Discuss, why doesn't this recognise the struct? is it because of the folder structure?
+
+	//UPROPERTY(BlueprintReadWrite, Category = "TopBarWidget")
+	//TObjectPtr<FEnemyWaveData> CurrentWaveData;
+
+	// Bound Widgets
+
+	UPROPERTY(BlueprintReadWrite, Category = "TopBarWidget", meta = (BindWidget))
+	TObjectPtr<UTextBlock> LivesText;
+
+	UPROPERTY(BlueprintReadWrite, Category = "TopBarWidget", meta = (BindWidget))
+	TObjectPtr<UTextBlock> MoneyText;
+
+	UPROPERTY(BlueprintReadWrite, Category = "TopBarWidget", meta = (BindWidget))
+	TObjectPtr<UTextBlock> WavesRemainingText;
+
+	UPROPERTY(BlueprintReadWrite, Category = "TopBarWidget", meta = (BindWidget))
+	TObjectPtr<UTextBlock> EnemiesRemainingText;
+
+	void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable, Category = "TopBarWidget",
+		meta = (ToolTip = "Updates values due to a new wave starting. Called when OnWaveChanged event is triggered"))
+	void NewWaveStarted(const int32 NewWavesRemaining, const int32 NewWaveEnemies);
+
+	UFUNCTION(BlueprintCallable, Category = "TopBarWidget",
+		meta = (ToolTip = "Updates the LivesText. Called when OnLivesChanged event is triggered"))
+	void UpdateLivesText(const int32 NewLives);
+
+	UFUNCTION(BlueprintCallable, Category = "TopBarWidget",
+		meta = (ToolTip = "Updates the MoneyText. Called when OnMoneyChanged event is triggered"))
+	void UpdateMoneyText(const int32 NewMoney);
+
+	UFUNCTION(BlueprintCallable, Category = "TopBarWidget",
+		meta = (ToolTip = "Updates the EnemiesRemainingText. Called when OnEnemiesRemainingChanged event is triggered"))
+	void UpdateEnemiesRemainingText(const int32 NewEnemiesRemaining);
 };
