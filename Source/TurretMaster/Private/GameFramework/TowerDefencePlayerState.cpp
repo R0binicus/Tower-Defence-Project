8a5562bb382947f9133a0ae1acbfd2594bf66103
Robin <robinandloki@gmail.com>
2025-11-08 15:26:32 +1100

    Basic Player State: TowerDefencePlayerState implementation
    - Setup TowerDefencePlayerState functions and variables
    - Created EPlayerStateEnum enum
    - Made FOnPlayerStateChanged for when the player state changes

--- Source/TurretMaster/Private/GameFramework/TowerDefencePlayerState.cpp
+++ Source/TurretMaster/Private/GameFramework/TowerDefencePlayerState.cpp
@@ -1,2 +1,26 @@
 #include "GameFramework/TowerDefencePlayerState.h"
 
+bool ATowerDefencePlayerState::TrySetPlayerState(const EPlayerStateEnum NewState)
+{
+    // This is excessive for what I actually need but oh well
+    switch (PlayerStateEnum)
+    {
+    case EPlayerStateEnum::Default:
+        SetPlayerState(NewState);
+        return true;
+        break;
+    case EPlayerStateEnum::Building:
+        SetPlayerState(NewState);
+        return true;
+        break;
+    default:
+        return false;
+        break;
+    }
+}
+
+void ATowerDefencePlayerState::SetPlayerState(const EPlayerStateEnum NewState)
+{
+    PlayerStateEnum = NewState;
+    OnPlayerStateChanged.Broadcast(PlayerStateEnum);
+}
--- Source/TurretMaster/Public/GameFramework/TowerDefencePlayerState.h
+++ Source/TurretMaster/Public/GameFramework/TowerDefencePlayerState.h
@@ -4,6 +4,15 @@
 #include "GameFramework/PlayerState.h"
 #include "TowerDefencePlayerState.generated.h"
 
+UENUM()
+enum class EPlayerStateEnum
+{
+	Default,
+	Building,
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStateChanged, EPlayerStateEnum, NewPlayerState);
+
 /**
  * Player state for the tower defence game
  */
@@ -11,5 +20,24 @@ UCLASS()
 class TURRETMASTER_API ATowerDefencePlayerState : public APlayerState
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "TowerDefencePlayerState")
+	FOnPlayerStateChanged OnPlayerStateChanged;
+
+	UFUNCTION(BlueprintCallable, Category = "TowerDefencePlayerState",
+		meta = (ToolTip = "Gets the TowerDefencePlayerState's PlayerStateEnum"))
+	EPlayerStateEnum GetPlayerState() const { return PlayerStateEnum; };
+
+	UFUNCTION(BlueprintCallable, Category = "TowerDefencePlayerState",
+		meta = (ToolTip = "Tries to set the player state to be the a new value"))
+	bool TrySetPlayerState(const EPlayerStateEnum NewState);
 	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TowerDefencePlayerState")
+	EPlayerStateEnum PlayerStateEnum;
+
+	UFUNCTION(BlueprintCallable, Category = "TowerDefencePlayerState",
+		meta = (ToolTip = "Updates player state and sends the event"))
+	void SetPlayerState(const EPlayerStateEnum NewState);
 };
