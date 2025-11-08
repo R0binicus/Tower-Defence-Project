bc2d3b362bd368fa358f59de57be14b317256670
Robin <robinandloki@gmail.com>
2025-11-08 14:32:26 +1100

    Basic Player State: Created TowerDefencePlayerState class

---
+++ Source/TurretMaster/Public/GameFramework/TowerDefencePlayerState.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "TowerDefencePlayerState.generated.h"
+
+/**
+ * Player state for the tower defence game
+ */
+UCLASS()
+class TURRETMASTER_API ATowerDefencePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
