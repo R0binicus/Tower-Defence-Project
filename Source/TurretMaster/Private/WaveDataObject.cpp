37e70331e46da20d6c9ab9571d0d76bc2bb5dd60
Robin <robinandloki@gmail.com>
2025-11-10 10:53:16 +1100

    Basic Player HUD: Created WaveDataObject class

---
+++ Source/TurretMaster/Private/WaveDataObject.cpp
@@ -0,0 +1,2 @@
+#include "WaveDataObject.h"
+
---
+++ Source/TurretMaster/Public/WaveDataObject.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "WaveDataObject.generated.h"
+
+/**
+ * An object to wrap around a EnemyWaveData, so it can be used with a pointer
+ */
+UCLASS()
+class TURRETMASTER_API UWaveDataObject : public UObject
+{
+	GENERATED_BODY()
+	
+};
