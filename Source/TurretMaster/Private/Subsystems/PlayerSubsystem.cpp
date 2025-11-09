42a64bfca7d04f26e6ea1475c3b42d07b88ee955
Robin <robinandloki@gmail.com>
2025-11-09 14:18:42 +1100

    Basic Player HUD: PlayerSubsystem setup and implementation

--- Source/TurretMaster/Private/Subsystems/PlayerSubsystem.cpp
+++ Source/TurretMaster/Private/Subsystems/PlayerSubsystem.cpp
@@ -1,2 +1,20 @@
 #include "Subsystems/PlayerSubsystem.h"
 
+void UPlayerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	PlayerLivesCurrent = PlayerLivesInitial;
+	PlayerMoneyCurrent = PlayerMoneyInitial;
+}
+
+void UPlayerSubsystem::SetPlayerLivesCurrent(const int32 NewLives)
+{
+	PlayerLivesCurrent = NewLives;
+	OnPlayerLivesChanged.Broadcast(PlayerLivesCurrent);
+}
+
+void UPlayerSubsystem::SetPlayerMoneyCurrent(const int32 NewMoney)
+{
+	PlayerMoneyCurrent = NewMoney;
+	OnPlayerMoneyChanged.Broadcast(PlayerMoneyCurrent);
+}
