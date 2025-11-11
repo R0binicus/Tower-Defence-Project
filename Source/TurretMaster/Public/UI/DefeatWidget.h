1f127dc764082697d8b785b614035f1b80e87b20
Robin <robinandloki@gmail.com>
2025-11-11 16:25:39 +1100

    Game End Screens: Created game end screen classes
    - Created DefeatWidget class
    - Created VictoryWiget class

---
+++ Source/TurretMaster/Public/UI/DefeatWidget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DefeatWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETMASTER_API UDefeatWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TurretMaster/Public/UI/VictoryWiget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "VictoryWiget.generated.h"
+
+/**
+ * Widget that appears when the player wins the game
+ */
+UCLASS()
+class TURRETMASTER_API UVictoryWiget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
