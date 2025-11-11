870a04a8ebb1681c45eeaba16f5f99c9c803ae85
Robin <robinandloki@gmail.com>
2025-11-11 12:36:15 +1100

    Lives System: Created TowerDefenceGameState class

---
+++ Source/TurretMaster/Private/GameFramework/TowerDefenceGameState.cpp
@@ -0,0 +1,2 @@
+#include "GameFramework/TowerDefenceGameState.h"
+
---
+++ Source/TurretMaster/Public/GameFramework/TowerDefenceGameState.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "TowerDefenceGameState.generated.h"
+
+/**
+ * Game state for the tower defence game.
+ * Triggers win + loss conditions
+ */
+UCLASS()
+class TURRETMASTER_API ATowerDefenceGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
