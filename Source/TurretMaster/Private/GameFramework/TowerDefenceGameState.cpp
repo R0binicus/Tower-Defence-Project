19a4e80fddcf16d631fb669e0ad948be5666995b
Robin <robinandloki@gmail.com>
2025-11-11 12:51:51 +1100

    Lives System: Game state implementation
    - Created game state win/lose functions and events

--- Source/TurretMaster/Private/GameFramework/TowerDefenceGameState.cpp
+++ Source/TurretMaster/Private/GameFramework/TowerDefenceGameState.cpp
@@ -1,2 +1,13 @@
 #include "GameFramework/TowerDefenceGameState.h"
 
+void ATowerDefenceGameState::TriggerWin()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("You Win!!! :)")));
+	OnGameWin.Broadcast();
+}
+
+void ATowerDefenceGameState::TwiggerLose()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("You Lose!!! :(")));
+	OnGameLose.Broadcast();
+}
--- Source/TurretMaster/Public/GameFramework/TowerDefenceGameState.h
+++ Source/TurretMaster/Public/GameFramework/TowerDefenceGameState.h
@@ -4,6 +4,9 @@
 #include "GameFramework/GameStateBase.h"
 #include "TowerDefenceGameState.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGameWin);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGameLose);
+
 /**
  * Game state for the tower defence game.
  * Triggers win + loss conditions
@@ -12,5 +15,19 @@ UCLASS()
 class TURRETMASTER_API ATowerDefenceGameState : public AGameStateBase
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "TowerDefenceGameState")
+	FOnGameWin OnGameWin;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "TowerDefenceGameState")
+	FOnGameLose OnGameLose;
 	
+	UFUNCTION(BlueprintCallable, Category = "BuildingSubsystem",
+		meta = (ToolTip = "Triggers the win condition, sending events to everything subscribed"))
+	void TriggerWin();
+
+	UFUNCTION(BlueprintCallable, Category = "BuildingSubsystem",
+		meta = (ToolTip = "Triggers the lose condition, sending events to everything subscribed"))
+	void TwiggerLose();
 };
