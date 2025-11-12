c9c3bc78c689cd57a3520f44dfb6dd668e467897
Robin <robinandloki@gmail.com>
2025-11-12 15:54:41 +1100

    Pause Menu: Created PauseMenuWidget class

---
+++ Source/TurretMaster/Public/UI/PauseMenuWidget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PauseMenuWidget.generated.h"
+
+/**
+ * Widget that appears when the player pauses the game
+ */
+UCLASS()
+class TURRETMASTER_API UPauseMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
