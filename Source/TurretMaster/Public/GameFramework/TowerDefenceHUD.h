a029169c3042f08706c6bc3893ac627f2a7e8cd8
Robin <robinandloki@gmail.com>
2025-11-12 11:19:00 +1100

    Game End Screens: Created TowerDefenceHUD class

---
+++ Source/TurretMaster/Public/GameFramework/TowerDefenceHUD.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TowerDefenceHUD.generated.h"
+
+/**
+ * HUD for the tower defence game.
+ */
+UCLASS()
+class TURRETMASTER_API ATowerDefenceHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
