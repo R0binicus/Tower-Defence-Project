ae2be5c38bb0c6dbc29084a0842d612df83edc3f
Robin <robinandloki@gmail.com>
2025-11-12 17:12:52 +1100

    Pause Menu: MainMenuWidget implementation

--- Source/TurretMaster/Private/UI/MainMenuWidget.cpp
+++ Source/TurretMaster/Private/UI/MainMenuWidget.cpp
@@ -1,2 +1,27 @@
 #include "UI/MainMenuWidget.h"
 
+void UMainMenuWidget::NativeConstruct()
+{
+	if (PlayButton)
+	{
+		PlayButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnPlayClicked);
+	}
+
+	if (QuitButton)
+	{
+		QuitButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnQuitClicked);
+	}
+}
+
+void UMainMenuWidget::OnPlayClicked()
+{
+	if (!FirstLevel.IsNull())
+	{
+		UGameplayStatics::OpenLevelBySoftObjectPtr(GetWorld(), FirstLevel);
+	}
+}
+
+void UMainMenuWidget::OnQuitClicked()
+{
+	UKismetSystemLibrary::QuitGame(this, UGameplayStatics::GetPlayerController(GetWorld(), 0), EQuitPreference::Quit, false);
+}
--- Source/TurretMaster/Public/UI/MainMenuWidget.h
+++ Source/TurretMaster/Public/UI/MainMenuWidget.h
@@ -2,14 +2,35 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
 #include "MainMenuWidget.generated.h"
 
 /**
- * 
+ * The primary widget in the main menu
  */
 UCLASS()
 class TURRETMASTER_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MainMenuWidget")
+	const TSoftObjectPtr<UWorld> FirstLevel;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MainMenuWidget", meta = (BindWidget))
+	TObjectPtr<UButton> PlayButton;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MainMenuWidget", meta = (BindWidget))
+	TObjectPtr<UButton> QuitButton;
+
+	void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable, Category = "MainMenuWidget",
+		meta = (ToolTip = "Starts the first level"))
+	void OnPlayClicked();
+
+	UFUNCTION(BlueprintCallable, Category = "MainMenuWidget",
+		meta = (ToolTip = "Quits the game application"))
+	void OnQuitClicked();
 };
