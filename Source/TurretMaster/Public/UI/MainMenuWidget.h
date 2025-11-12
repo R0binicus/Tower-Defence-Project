ddf655db08b588a27441898ae6d239fccc975a92
Robin <robinandloki@gmail.com>
2025-11-12 17:10:48 +1100

    Pause Menu: Created MainMenuWidget class

---
+++ Source/TurretMaster/Public/UI/MainMenuWidget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETMASTER_API UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
