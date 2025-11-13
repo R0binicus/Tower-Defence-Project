a31aae3ad641724f6e9411d3621d2649b2571a53
Robin <robinandloki@gmail.com>
2025-11-13 15:48:18 +1100

    Basic Player Inputs: Player controller implementation

--- Source/TurretMaster/Private/GameFramework/TowerDefencePlayerController.cpp
+++ Source/TurretMaster/Private/GameFramework/TowerDefencePlayerController.cpp
@@ -1,2 +1,47 @@
 #include "GameFramework/TowerDefencePlayerController.h"
 
+void ATowerDefencePlayerController::BeginPlay()
+{
+
+}
+
+void ATowerDefencePlayerController::SetupInputComponent()
+{
+	APlayerController::SetupInputComponent();
+
+	TObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (!EnhancedInputComponent)
+	{
+		return;
+	}
+
+	if (PauseAction)
+	{
+		EnhancedInputComponent->BindAction(PauseAction, ETriggerEvent::Started, this, &ATowerDefencePlayerController::PauseInputAction);
+	}
+
+	if (QueueAction)
+	{
+		EnhancedInputComponent->BindAction(QueueAction, ETriggerEvent::Started, this, &ATowerDefencePlayerController::QueueInputAction);
+	}
+
+	if (CancelAction)
+	{
+		EnhancedInputComponent->BindAction(CancelAction, ETriggerEvent::Started, this, &ATowerDefencePlayerController::CancelInputAction);
+	}
+}
+
+void ATowerDefencePlayerController::PauseInputAction()
+{
+	OnPauseInput.Broadcast();
+}
+
+void ATowerDefencePlayerController::QueueInputAction()
+{
+	OnQueueInput.Broadcast();
+}
+
+void ATowerDefencePlayerController::CancelInputAction()
+{
+	OnCancelInput.Broadcast();
+}
--- Source/TurretMaster/Public/GameFramework/TowerDefencePlayerController.h
+++ Source/TurretMaster/Public/GameFramework/TowerDefencePlayerController.h
@@ -2,7 +2,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 #include "TowerDefencePlayerController.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPauseInput);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnQueueInput);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCancelInput);
 
 /**
  * Player Controller for the tower defence game.
@@ -11,5 +18,42 @@ UCLASS()
 class TURRETMASTER_API ATowerDefencePlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "TowerDefencePlayerController")
+	FOnPauseInput OnPauseInput;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "TowerDefencePlayerController")
+	FOnQueueInput OnQueueInput;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "TowerDefencePlayerController")
+	FOnCancelInput OnCancelInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TowerDefencePlayerController")
+	TObjectPtr<UInputMappingContext> InputMap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TowerDefencePlayerController")
+	TObjectPtr<UInputAction> PauseAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TowerDefencePlayerController")
+	TObjectPtr<UInputAction> QueueAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TowerDefencePlayerController")
+	TObjectPtr<UInputAction> CancelAction;
+
+	void BeginPlay() override;
+
+	void SetupInputComponent() override;
+
+	UFUNCTION(BlueprintCallable, Category = "TowerDefencePlayerController",
+		meta = (ToolTip = "Broadcasts an event saying that the pause button was pressed"))
+	void PauseInputAction();
+
+	UFUNCTION(BlueprintCallable, Category = "TowerDefencePlayerController",
+		meta = (ToolTip = "Broadcasts an event saying that the queue button was pressed"))
+	void QueueInputAction();
+
+	UFUNCTION(BlueprintCallable, Category = "TowerDefencePlayerController",
+		meta = (ToolTip = "Broadcasts an event saying that the cancel button was pressed"))
+	void CancelInputAction();
 };
