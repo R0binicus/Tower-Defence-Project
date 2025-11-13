d398a7b6257bbd9ebd19e45e7cbc36ea5bb46c84
Robin <robinandloki@gmail.com>
2025-11-13 14:42:23 +1100

    Basic Player Inputs: Created TowerDefencePlayerController class

---
+++ Source/TurretMaster/Public/GameFramework/TowerDefencePlayerController.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TowerDefencePlayerController.generated.h"
+
+/**
+ * Player Controller for the tower defence game.
+ */
+UCLASS()
+class TURRETMASTER_API ATowerDefencePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
