a157025f7ddd0c6b6b9503a65401c377cc3a5861
Robin <robinandloki@gmail.com>
2025-11-15 14:50:09 +1100

    Enemy Health Bars: Created EnemyHealthbarWidget class

---
+++ Source/TurretMaster/Public/UI/EnemyHealthbarWidget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EnemyHealthbarWidget.generated.h"
+
+/**
+ * Health bar widget, which displays an enemy's health over its head
+ */
+UCLASS()
+class TURRETMASTER_API UEnemyHealthbarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
