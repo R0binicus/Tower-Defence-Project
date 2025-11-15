bafb519b20fa11c08e3576a344b9d357f6beb327
Robin <robinandloki@gmail.com>
2025-11-15 17:35:38 +1100

    Enemy Health Bars: Created LevelDataAsset class

---
+++ Source/TurretMaster/Public/PrimaryAssets/LevelDataAsset.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelDataAsset.generated.h"
+
+/**
+ * Primary data asset for storing level data
+ */
+UCLASS()
+class TURRETMASTER_API ULevelDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
