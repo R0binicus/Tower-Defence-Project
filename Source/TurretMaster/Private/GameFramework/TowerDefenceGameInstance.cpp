22d2e4a9212c51e8f29eac3adfb67e8817ea6db1
Robin <robinandloki@gmail.com>
2025-11-15 20:50:09 +1100

    Level Data Asset: Created TowerDefenceGameInstance class

---
+++ Source/TurretMaster/Private/GameFramework/TowerDefenceGameInstance.cpp
@@ -0,0 +1,1 @@
+#include "GameFramework/TowerDefenceGameInstance.h"
---
+++ Source/TurretMaster/Public/GameFramework/TowerDefenceGameInstance.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TowerDefenceGameInstance.generated.h"
+
+/**
+ * Game instance for the tower defence game
+ * Primarily handles loading if level data for each level
+ */
+UCLASS()
+class TURRETMASTER_API UTowerDefenceGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
