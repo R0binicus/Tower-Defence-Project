6fb401c5b4017e84450755ee47327390e8c396c7
Robin <robinandloki@gmail.com>
2025-11-22 14:17:43 +1100

    Building Info Display: Created BuildingInfoDisplayWidget class

---
+++ Source/TurretMaster/Public/UI/BuildingInfoDisplayWidget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BuildingInfoDisplayWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURRETMASTER_API UBuildingInfoDisplayWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
