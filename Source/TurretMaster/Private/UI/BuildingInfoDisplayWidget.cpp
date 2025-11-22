6aa4d5a8c7d93e4928902dc99c637dbe48b8fbd2
Robin <robinandloki@gmail.com>
2025-11-22 15:42:08 +1100

    Building Info Display: BuildingInfoDisplayWidget declarations
    - Setup BuildingInfoDisplayWidget variable and function declarations

--- Source/TurretMaster/Private/UI/BuildingInfoDisplayWidget.cpp
+++ Source/TurretMaster/Private/UI/BuildingInfoDisplayWidget.cpp
@@ -1,2 +1,20 @@
 #include "UI/BuildingInfoDisplayWidget.h"
+#include "Components/TextBlock.h"
+#include "Subsystems/BuildingSubsystem.h"
+#include "DataAssets/BuildingDataAsset.h"
+#include "Turret.h"
 
+void UBuildingInfoDisplayWidget::NativeConstruct()
+{
+
+}
+
+void UBuildingInfoDisplayWidget::UpdateBuildingInfoDisplay(const UBuildingDataAsset* BuildingData, const ATurret* Turret)
+{
+
+}
+
+void UBuildingInfoDisplayWidget::HideBuildingDisplay()
+{
+
+}
--- Source/TurretMaster/Public/UI/BuildingInfoDisplayWidget.h
+++ Source/TurretMaster/Public/UI/BuildingInfoDisplayWidget.h
@@ -4,12 +4,34 @@
 #include "Blueprint/UserWidget.h"
 #include "BuildingInfoDisplayWidget.generated.h"
 
+class UTextBlock;
+class UBuildingSubsystem;
+class UBuildingDataAsset;
+class ATurret;
+
 /**
- * 
+ * Widget to display building information
  */
 UCLASS()
 class TURRETMASTER_API UBuildingInfoDisplayWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+
+	UPROPERTY(BlueprintReadWrite, Category = "BuildingInfoDisplayWidget", meta = (BindWidget))
+	TObjectPtr<UTextBlock> BuildingName;
+
+	UPROPERTY(BlueprintReadWrite, Category = "BuildingInfoDisplayWidget", meta = (BindWidget))
+	TObjectPtr<UTextBlock> BuildingDesc;
+
+	void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable, Category = "BuildingInfoDisplayWidget",
+		meta = (ToolTip = "Updates the information in the building info display"))
+	void UpdateBuildingInfoDisplay(const UBuildingDataAsset* BuildingData, const ATurret* Turret);
+
+	UFUNCTION(BlueprintCallable, Category = "BuildingInfoDisplayWidget",
+		meta = (ToolTip = "Hides the building display text and buttons"))
+	void HideBuildingDisplay();
 };
