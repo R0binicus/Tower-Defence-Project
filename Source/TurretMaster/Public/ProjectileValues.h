d913798478069033272559d0b396fe6b7f736124
Robin <robinandloki@gmail.com>
2025-11-23 17:09:26 +1100

    Mortar Gun: Added HitExplosionRadius projectile value

--- Source/TurretMaster/Public/ProjectileValues.h
+++ Source/TurretMaster/Public/ProjectileValues.h
@@ -26,15 +26,18 @@ struct FProjectileValues
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Values")
 	float TurnMultiplier;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile Values")
+	float HitExplosionRadius;
+
 	UPROPERTY(BlueprintReadWrite, Category = "Projectile Values")
 	float PredictedLifetime;
 
-	FProjectileValues() : Damage(10.f), Speed(1000.f), Lifetime(10.f), Scale(1.f), TurnMultiplier(1.f), PredictedLifetime(2.f)
+	FProjectileValues() : Damage(10.f), Speed(1000.f), Lifetime(10.f), Scale(1.f), TurnMultiplier(1.f), HitExplosionRadius(0.f), PredictedLifetime(2.f)
 	{
 	}
 
-	FProjectileValues(const float InDamage, const float InSpeed, const float InLifetime, const float InScale, const float InTurnMultiplier, const float InPredictedLifetime = 2) :
-		Damage(InDamage), Speed(InSpeed), Lifetime(InLifetime), Scale(InScale), TurnMultiplier(InTurnMultiplier), PredictedLifetime(InPredictedLifetime)
+	FProjectileValues(const float InDamage, const float InSpeed, const float InLifetime, const float InScale, const float InTurnMultiplier, const float InHitExplosionRadius = 0.f, const float InPredictedLifetime = 2.f) :
+		Damage(InDamage), Speed(InSpeed), Lifetime(InLifetime), Scale(InScale), TurnMultiplier(InTurnMultiplier), HitExplosionRadius(InHitExplosionRadius), PredictedLifetime(InPredictedLifetime)
 	{
 	}
 };
