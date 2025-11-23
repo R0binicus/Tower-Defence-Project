8ed65a26e94f63b47f6697d1f2b81f2e59eb1ff4
Robin <robinandloki@gmail.com>
2025-11-23 15:51:10 +1100

    UI Adjustments: Created explosive projectile class

---
+++ Source/TurretMaster/Public/Projectiles/ExplosiveProjectile.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile.h"
+#include "ExplosiveProjectile.generated.h"
+
+/**
+ * Variant of projectile, which damages enemies in an area when it hits surface or enemy
+ */
+UCLASS()
+class TURRETMASTER_API AExplosiveProjectile : public AProjectile
+{
+	GENERATED_BODY()
+	
+};
