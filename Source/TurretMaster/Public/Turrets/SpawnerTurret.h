5720fa416b0042e4d8a8523bee4db9ad999d2333
Robin <robinandloki@gmail.com>
2025-11-26 23:24:43 +1100

    Tesla Turret: Created SpawnerTurret class

---
+++ Source/TurretMaster/Public/Turrets/SpawnerTurret.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Turret.h"
+#include "SpawnerTurret.generated.h"
+
+/**
+ * Turret variant which spawns other objects with set lifetimes
+ */
+UCLASS()
+class TURRETMASTER_API ASpawnerTurret : public ATurret
+{
+	GENERATED_BODY()
+	
+};
